4239c1f98e945278f0c55c805346ccda3901281b
choiyunseo <choeyunseo4060@naver.com>
2026-02-18 17:28:30 +0900

    Feat: +,- 연산 가정, 괄호 제외

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -2,22 +2,57 @@ package com.ll;
 
 public class Calc {
     public static int run(String cmd) {
-        /* t1 ~ t7 */
-        // expr 공백 없애기
-        String expr = cmd.replace(" ", "");
+        cmd = cmd.replace(" ", "");
+        if (cmd.isEmpty()) return 0;
 
-        // == 기준 분리
-        String[] sides = expr.split("=="); // sides = ["1+1", "2"]
-        // 사칙연산자 기준 분리
-        String[] tokens = sides[0].split("(?<=[+\\-*/])|(?=[+\\-*/])"); // tokens = ["1", "+", "1]
+        // 첫항 기호 붙이기 (자릿수 맞추기 위함)
+        if (cmd.charAt(0) != '+' && cmd.charAt(0) != '-') {
+            cmd = "+" + cmd;
+        }
 
-        if (tokens[1].equals("+")) {
-            // 정수화
-            return Integer.parseInt(tokens[0]) + Integer.parseInt(tokens[2]);
+        // +, - 연산자 기준으로 쪼개기
+        int idx = 1;
+        // 숫자 or * or / 이면
+        while (idx < cmd.length() && cmd.charAt(idx) != '+' && cmd.charAt(idx) != '-') {
+            idx++;
         }
-        else if (tokens[1].equals("-")) {
-            return Integer.parseInt(tokens[0]) - Integer.parseInt(tokens[2]);
+
+        String left = cmd.substring(0, idx); // *, /만 포함
+        String right = cmd.substring(idx);
+
+        // 왼쪽 항 계산
+        int sum = calculate(left);
+        return sum + run(right);
+    }
+
+    public static int calculate(String expr) {
+        // 예외사항
+        if (expr.length() < 2) {
+            return 0;
         }
-        return 0;
+
+
+        // 자릿수 말고 연산자 기준으로 나누기 (모든 숫자가 일의 자리가 아님)
+        char sign = expr.charAt(0);
+        String body = expr.substring(1);
+
+        int result;
+
+        // 곱하기
+        if (body.contains("*")) {
+            String[] parts = body.split("\\*");
+            result = Integer.parseInt(parts[0]) * Integer.parseInt(parts[1]);
+        }
+        // 나누기
+        else if (body.contains("/")) {
+            String[] parts = body.split("/");
+            result = Integer.parseInt(parts[0]) / Integer.parseInt(parts[1]);
+        }
+        // 그냥 숫자면
+        else {
+            result = Integer.parseInt(body);
+        }
+
+        return sign == '-' ? -result : result;
     }
 }
